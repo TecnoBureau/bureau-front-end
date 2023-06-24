ca7b4e0b1738da97eff56069b9dec0b7b8752dab
Gabriel-Paes <paessouzagabriel@gmail.com>
2023-06-23 21:41:42 -0300

    add content in inscriptionsComponent

--- src/app/inscriptions/inscriptions.component.ts
+++ src/app/inscriptions/inscriptions.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { ContainerComponent } from '../ui/container/container.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-inscriptions',
   templateUrl: './inscriptions.component.html',
   styleUrls: ['./inscriptions.component.css'],
   standalone: true,
+  imports: [ContainerComponent, MatDividerModule, MatButtonModule],
 })
 export class InscriptionsComponent {}
